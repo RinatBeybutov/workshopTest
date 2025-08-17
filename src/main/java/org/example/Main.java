0a9b97d341a3d8c202aaf4d7feca3ae2eba5e9c2
Ринат <ignat934@gmail.com>
2025-08-17 17:09:07 +0300

    1: - добавлено создание задачи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
 
+    TodoManager manager = new TodoManagerImpl();
+
+    manager.create("первая задача", false);
   }
 }
---
+++ src/main/java/org/example/TodoManager.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+/**
+ * Класс для управления задачами
+ *
+ * @author Gaben 17.08.2025
+ */
+public interface TodoManager {
+
+  void create(String title, boolean isCompleted);
+}
